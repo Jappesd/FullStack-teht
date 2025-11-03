fa657c2ce7ce5c3134165d1c5c480920da3b6356
S.K <ksamppa@hotmail.com>
2025-11-03 12:27:13 +0200

    Update index.js

--- kurssiharjoittelu/index.js
+++ kurssiharjoittelu/index.js
@@ -11,7 +11,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 let notes = [
