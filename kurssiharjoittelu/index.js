0c629e274045b874dce5b491b0b8389796b7c64e
S.K <ksamppa@hotmail.com>
2025-11-03 12:20:48 +0200

    Update index.js

--- kurssiharjoittelu/index.js
+++ kurssiharjoittelu/index.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
