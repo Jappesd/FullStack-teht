20325f334e9fe9f61465735a39f89e97c0c1d738
S.K <ksamppa@hotmail.com>
2025-11-03 10:53:49 +0200

    backend change

--- kurssiharjoittelu/src/services/notes.js
+++ kurssiharjoittelu/src/services/notes.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-const baseUrl = 'http://localhost:3001/notes'
+const baseUrl = "http://localhost:3001/api/notes";
 
 const getAll = () => {
-    return axios.get(baseUrl)
-}
+  return axios.get(baseUrl);
+};
 const create = (newObject) => {
-    return axios.post(baseUrl, newObject)
-}
+  return axios.post(baseUrl, newObject);
+};
 
 const update = (id, newObject) => {
-    return axios.put(`${baseUrl}/${id}`, newObject)
-}
+  return axios.put(`${baseUrl}/${id}`, newObject);
+};
 
-export default {getAll, create, update}
+export default { getAll, create, update };
