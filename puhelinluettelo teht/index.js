7a2cc23fd15588edba034efd3f72ca308df88e23
S.K <ksamppa@hotmail.com>
2025-11-07 12:39:13 +0200

    preparing for mongo

--- puhelinluettelo teht/index.js
+++ puhelinluettelo teht/index.js
@@ -1,20 +1,25 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const Person = (await import("./models/person.js")).default;
+console.log("Person import", Person);
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("dist"));
 
-let persons = [
-  { id: 1, name: "Arto Hellas", number: "040-123456" },
-  { id: 2, name: "Ada Lovelace", number: "39-44-5323523" },
-  { id: 3, name: "Dan Abramov", number: "12-43-234345" },
-  { id: 4, name: "Mary Poppendieck", number: "39-23-6423122" },
-];
-
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then((persons) => {
+    res.json(persons);
+  });
 });
 
 app.get("/info", (req, res) => {
@@ -23,14 +28,16 @@ app.get("/info", (req, res) => {
     `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
   );
 });
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((p) => p.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((err) => next(err));
 });
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id);
@@ -52,9 +59,11 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-app.use((req, res) => {
-  res.sendFile(__dirname + "/dist/index.html");
-});
+app.use(errorHandler);
+
+// app.use((req, res) => {
+//   res.sendFile(__dirname + "/dist/index.html");
+// });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
---
+++ puhelinluettelo teht/models/person.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const url = process.env.MONGODB_URI;
+console.log("Connecting to MongoDB");
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log("error connecting", err.message));
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+export default Person;
