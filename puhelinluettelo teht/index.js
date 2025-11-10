27326e840176495d76dd2bef3d570e3c607ab717
S.K <ksamppa@hotmail.com>
2025-11-10 12:43:31 +0200

    Update index.js

--- puhelinluettelo teht/index.js
+++ puhelinluettelo teht/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 dotenv.config();
 const Person = (await import("./models/person.js")).default;
 console.log("Person import", Person);
@@ -8,6 +10,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "dist")));
 
 const errorHandler = (error, req, res, next) => {
   console.error(error.message);
@@ -105,9 +110,9 @@ app.put("/api/persons/:id", async (req, res, next) => {
 
 app.use(errorHandler);
 
-// app.use((req, res) => {
-//   res.sendFile(__dirname + "/dist/index.html");
-// });
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
