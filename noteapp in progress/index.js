754525b7ba209033b3a51a211371d32ed4456a84
S.K <ksamppa@hotmail.com>
2025-11-25 09:17:27 +0200

    Update index.js

--- noteapp in progress/index.js
+++ noteapp in progress/index.js
@@ -17,7 +17,7 @@ mongoose
     app.use(express.static(path.join(__dirname, "dist")));
 
     // 2. SPA fallback
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
       res.sendFile(path.join(__dirname, "dist", "index.html"));
     });
 
