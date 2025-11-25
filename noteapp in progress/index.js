1139659d1da61d2d1a292c44691297e41f4cbd44
S.K <ksamppa@hotmail.com>
2025-11-25 09:12:44 +0200

    Update index.js

--- noteapp in progress/index.js
+++ noteapp in progress/index.js
@@ -8,11 +8,20 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "dist")));
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     logger.info("Connected to mongoDB");
+
+    // 1. Serve frontend
+    app.use(express.static(path.join(__dirname, "dist")));
+
+    // 2. SPA fallback
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "dist", "index.html"));
+    });
+
+    // 3. Start server
     app.listen(process.env.PORT, () => {
       logger.info(`Server running on port ${process.env.PORT}`);
     });
@@ -20,7 +29,3 @@ mongoose
   .catch((err) => {
     logger.error("Error connecting to MongoDB", err.message);
   });
-
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
-});
